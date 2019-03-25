0629db71d1403f3f16520b44a913925d7e834df9
Mahanoor <2015csb1031@iitrpr.ac.in>
2019-03-25 22:51:49 +0530

    Updated UI

---
+++ config/routes.rb
@@ -0,0 +1,18 @@
+Rails.application.routes.draw do
+
+
+
+	
+ 
+  devise_for :users
+    
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+    get 'api/index'
+    get 'api/dashboard'
+    root to: "api#index"
+
+   devise_scope :user do  
+   get '/users/sign_out' => 'devise/sessions#destroy'     
+	end
+   
+end
